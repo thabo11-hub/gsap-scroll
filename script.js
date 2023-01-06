142c2e6390181bbeb3984b4fb9e92d00a746322f
Thabo Sodi <thabosodi11@gmail.com>
2023-01-06 16:58:37 +0200

    all js

--- script.js
+++ script.js
@@ -13,26 +13,61 @@ ScrollTrigger.create({
   trigger: "#container",
   pin: true,
   start: "top top",
-  end: "+=5700"
+  end: "+=5700",
 });
 
-gsap.set(".specs",{
-    x: -160,
-    opacity: 0
+gsap.set(".specs", {
+  x: -160,
+  opacity: 0,
 });
 
-gsap.set(".chars",{
-    x: 260
+gsap.set(".chars", {
+  x: 260,
 });
 
-part2_t1.set(".models li",{
-    opacity: 0
+part2_t1.set(".models li", {
+  opacity: 0,
 });
 
-part3_t1.set(".specs dt",{
-    opacity: 0
+part3_t1.set(".specs dt", {
+  opacity: 0,
 });
 
-part3_t1.set(".specs dd",{
-    opacity: 0
+part3_t1.set(".specs dd", {
+  opacity: 0,
 });
+
+intro_t1
+  .fromTo(
+    "#wrapWin",
+    {
+      height: 80,
+    },
+    {
+      height: 800,
+      duration: 0.1,
+    }
+  )
+  .fromTo(
+    "liberty",
+    {
+      scale: 0.8,
+      y: -300,
+    },
+    {
+      scale: 1,
+      y: 0,
+      duration: 0.1,
+    }
+  )
+  .to("#logo", {
+    scrollTrigger: {
+      start: 300,
+      end: 500,
+      scrub: 0.5,
+    },
+    y: -190,
+    scale: 0.6,
+    duration: 0.6,
+    ease: "expo.out",
+  })
